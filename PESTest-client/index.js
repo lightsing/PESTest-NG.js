9f0aa886fd9ec5dc7d30a9661792a0edc532c1b5
lightsing <light.tsing@gmail.com>
2017-03-26 16:25:04 +0800

    Rename PESTest-client to PESTest-api

--- PESTest-client/index.js
+++ PESTest-client/index.js
@@ -2,4 +2,4 @@
 
 exports.PESTAgent = require('./PESTest-agent').PESTAgent;
 exports.PESTxml = require('./PESTest-xml').PESTxml;
-exports.PESTApi = require('./PESTest-client').PESTApi;
+exports.PESTApi = require('./PESTest-api').PESTApi;

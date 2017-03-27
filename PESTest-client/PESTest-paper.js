75dde4b440c422c12d80d4e665eb8630c0d0b828
lightsing <light.tsing@gmail.com>
2017-03-27 16:07:43 +0800

    Add getAns method

--- PESTest-client/PESTest-paper.js
+++ PESTest-client/PESTest-paper.js
@@ -7,6 +7,29 @@ class PESTPaper {
     this.xml = xml;
     this.unescape();
     this.json = convert.xml2js(this.xml);
+
+    this.info = this.json.elements[0].elements[0].elements;
+    this.paperName = this.info[1].elements[0].text;
+    this.fullScore = this.info[5].elements[0].text;
+
+    this.questions = this.json.elements[0].elements[1].elements;
+  }
+
+  getAns() {
+    var ans = [];
+    this.questions.forEach( function(question) {
+      var stdAnswer = '';
+      if (question.attributes.Type !== 'OP') {
+        question.elements.forEach( function(child) {
+          if (child.name == 'StdAnswer') {
+            stdAnswer = child.elements[0].text;
+          }
+        });
+      }
+      ans.push(stdAnswer);
+    });
+    this.ans = ans;
+    return ans;
   }
 
   escape() {

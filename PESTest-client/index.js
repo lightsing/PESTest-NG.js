09465e8c69804121d7999f685019f6887178a319
lightsing <light.tsing@gmail.com>
2017-04-01 23:09:00 +0800

    Update struct

--- PESTest-client/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-
-exports.PESTAgent = require('./PESTest-agent').PESTAgent;
-exports.PESTxml = require('./PESTest-xml').PESTxml;
-exports.PESTApi = require('./PESTest-api').PESTApi;
-exports.PESTClient = require('./PESTest-client').PESTClient;
---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+exports.PESTAgent = require('./lib/PESTest-agent').PESTAgent;
+exports.PESTxml = require('./lib/PESTest-xml').PESTxml;
+exports.PESTApi = require('./lib/PESTest-api').PESTApi;
+exports.PESTClient = require('./lib/PESTest-client').PESTClient;
